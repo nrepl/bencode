37ec1fcf768a0c004381924a21f733d34d7f92de
Bozhidar Batsov <bozhidar@batsov.com>
2018-06-17 21:01:23 +0300

    Improve the release management

--- project.clj
+++ project.clj
@@ -3,4 +3,18 @@
   :url "https://github.com/nrepl/bencode"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+
+  :aliases {"bump-version" ["change" "version" "leiningen.release/bump-version"]}
+
+  :release-tasks [["vcs" "assert-committed"]
+                  ["bump-version" "release"]
+                  ["vcs" "commit" "Release %s"]
+                  ["vcs" "tag" "v" "--no-sign"]
+                  ["bump-version"]
+                  ["vcs" "commit" "Begin %s"]]
+
+  :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
+                                    :username :env/clojars_username
+                                    :password :env/clojars_password
+                                    :sign-releases false}]])
